f8dfad8a4ceffdb52e932a5073b783873209c2e7
Axel Esquite <axel@socialbomb.com>
2012-05-10 17:36:25 -0400

    made some changes to the template to show the proper requirements of cards

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-CleenCards is a Javascript helper tool that formats a csv of card attributes into printable cards I can use for game testing.
+CleenCards
+==========
+A Javascript helper tool that formats a csv of card attributes into printable cards I can use for game testing.
+
+CleenCards runs on [Tornado](http://www.tornadoweb.org/). In order to run it you'll have to
+       cd CleenCards
+       python main.py --port=3000
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
-var prod_card = '{{#each production}}<li><div class="points">{{points}}</div><h2>{{title}}</h2><p>{{description}}</p></li>{{/each}}';
-var action_card = '{{#each actions}}<li><h2>{{title}}</h2><img src="{{image}}" width="150" height="150" /><p>{{description}}</p></li>{{/each}}';
-var card = '{{#each actions}}<li><h2>{{title}}</h2><p>{{description}}</p></li>{{/each}}';
+var productionCardTemplate;
+var actionCardTemplate;
+var cardTemplate;
 require.config({
 	baseUrl: '/static/js',
 	paths: {
@@ -14,13 +14,15 @@ require(['require', 'jquery', 'libs/respond.min', 'plugins', 'libs/handlebars', 
 
 
 function onDependsLoaded(req,$,modernizer,respond,plugins,csv,SBMediator) {
-		// $.get('static/csv/ProductionCards.csv', onProductionCSVLoaded);
+		cardTemplate = Handlebars.compile('{{#each actions}}<li><h2>{{title}}</h2><p>{{description}}</p></li>{{/each}}');
+		productionCardTemplate = Handlebars.compile('{{#each production}}<li><div class="points">{{points}}</div><h2>{{title}}</h2>{{#if description}}<p>{{description}}</p>{{/if}}{{#if turns}}<div class="turns">Turns: {{turns}}</div>{{/if}}{{#if sheep_cost}}<div class="cost">Sheep cost: {{sheep_cost}}</div>{{/if}}</li>{{/each}}');
+		actionCardTemplate = Handlebars.compile('{{#each actions}}<li><h2>{{title}}</h2><img src="{{image}}" width="150" height="150" /><p>{{description}}</p></li>{{/each}}');
+		$.get('static/csv/ProductionCards.csv', onProductionCSVLoaded);
 		$.get('static/csv/ActionsCards.csv', onActionCSVLoaded);
 }
 
 function onProductionCSVLoaded(d)
 {
-	var template = Handlebars.compile(prod_card);
 	var dict = $.csv2Dictionary(d);
 	var data = {production:[]};
 	var img = 'static/img/placeholder.png';
@@ -31,22 +33,25 @@ function onProductionCSVLoaded(d)
 		{
 			for (var j = parseInt(dict[i].quantity,10) - 1; j >= 0; j--)
 			{
-				data.production.push({title: dict[i].name, description: dict[i].description, points: dict[i].point_value});
-				//log(dict[i], data.cards[data.cards.length-1]);
+				var item = {title: dict[i].name,
+					points: dict[i].point_value};
+
+				if(dict[i].description !== '') { item.description = dict[i].description; }
+				if(parseInt(dict[i].turn_requirements,10) > 0) { item.turns = dict[i].turn_requirements; }
+				if(parseInt(dict[i].sheep_requirements,10) > 0) { item.sheep_cost = dict[i].sheep_requirements; }
+				data.production.push(item);
 			}
 		}
 	}
-	
-	$('#production_cards').html(template(data));
+	renderCard(data, 'PRODUCTION');
 }
 
 function onActionCSVLoaded(d)
 {
 	//log('onActionCSVLoaded', d);
-	var template = Handlebars.compile(action_card);
 	var dict = $.csv2Dictionary(d);
 	var data = {actions:[]};
-	var img = 'static/img/placeholder.png';
+	
 	log('onActionCSVLoaded', dict);
 	for (var i = dict.length - 1; i >= 0; i--)
 	{
@@ -54,13 +59,13 @@ function onActionCSVLoaded(d)
 		{
 			for (var j = parseInt(dict[i].quantity,10) - 1; j >= 0; j--)
 			{
-				data.actions.push({title: dict[i].name, image: img, description: dict[i].description});
-				//log(dict[i], data.cards[data.cards.length-1]);
+				data.actions.push({title: dict[i].name, 
+					image: dict[i].image, 
+					description: dict[i].description});
 			}
 		}
 	}
-
-	$('#action_cards').html(template(data));
+	renderCard(data, 'ACTION');
 }
 
 function renderCard(dict, card_type)
@@ -70,11 +75,11 @@ function renderCard(dict, card_type)
 	switch(card_type)
 	{
 		case 'PRODUCTION':
-			$('#production_cards').html(template(data));
+			$('#production_cards').html(productionCardTemplate(dict));
 		break;
 
 		case 'ACTION':
-			$('#action_cards').html(template(data));
+			$('#action_cards').html(actionCardTemplate(dict));
 		break;
 	}
 }
